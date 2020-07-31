d6096f6d34db9748a44946d7be311df01989f4f8
Carl Holmquist <cwv.holmquist@gmail.com>
2020-07-31 13:32:37 -0500

    broken up homepage into components and styled them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './page/homepage/homepage.component.jsx'
 
 function App() {
   return (

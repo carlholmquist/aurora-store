59410947a207199dd2cb0cc99e49ef57ac58ea99
Carl Holmquist <cwv.holmquist@gmail.com>
2020-08-01 16:50:03 -0500

    created header component and internal nav-links wiht router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
 import { Route, Switch } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>

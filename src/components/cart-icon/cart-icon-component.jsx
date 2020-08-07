0dc0dce99ddbe8e8c2e726c26a119cbe25e1fe63
Carl Holmquist <cwv.holmquist@gmail.com>
2020-08-07 14:04:03 -0500

    implemented cart selectors with reselect library

--- src/components/cart-icon/cart-icon-component.jsx
+++ src/components/cart-icon/cart-icon-component.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({toggleCartHidden, cartItems}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'> {cartItems ? cartItems.reduce((acc,current) => acc + current.quantity, 0) : 0 } </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 )
 
-const mapStateToProps = ({ cart }) => ({
-    cartItems: cart.cartItems
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state) 
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({imageUrl, id, name, quantity}) => (
+const CartItem = ({imageUrl, id, name, quantity, price}) => (
     <div className='cart-item'>
         <div 
             className='image'
             style={{backgroundImage: `url(${imageUrl})`}}
         />
-        <span className='title'> { name }</span>
-        <span className='counter'> {quantity} </span>
+        <div className='text-container'>
+            <span className='title'> { name }</span>
+            <span className='counter'> {quantity} x {price}</span>
+        </div>
 
     </div>
 )

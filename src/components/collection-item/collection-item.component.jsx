025db0261c007cb9ef26b98e702d57cf5818e21e
Carl Holmquist <cwv.holmquist@gmail.com>
2020-08-06 21:17:12 -0500

    added button to collection items, and implemented adding to cart functionality to button with cartReducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItemToCart } from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItemToCart}) => {
+    
+    const { name, price, imageUrl} = item;
+
+    return(
     <div className='collection-item'>
         <div 
             className='image'
@@ -11,7 +20,12 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>${price}</span>
         </div>
+        <CustomButton inverted onClick={() => addItemToCart(item)}>Add to cart</CustomButton>
     </div>
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItemToCart: item => dispatch(addItemToCart(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = bool => ({
     type: cartActionTypes.TOGGLE_CART_HIDDEN,
     payload: bool
 })
+
+export const addItemToCart = item => ({
+    type: cartActionTypes.ADD_ITEM,
+    payload: item
+})
